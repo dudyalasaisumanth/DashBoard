031dd0450c369e17a7fddc71181dc27afdf3a79f
sumpored <sumpored@cisco.com>
2020-01-22 21:42:31 +0530

    add service ts file

--- src/app/file-service.service.ts
+++ src/app/file-service.service.ts
@@ -11,7 +11,7 @@ export class FileServiceService {
   constructor(private http:HttpClient) { }
   
   TriggerMail(data){
-    this.http.post<any>('url here', data).subscribe(data => {
+    this.http.post<any>('http://0.0.0.0:8888/sendemail', data).subscribe(data => {
     console.log(data)
 })
   }
